7c64ca38622fd8b11cf395536a95c47247c12d8b
Bill Jackson <wjackson@mos.org>
2019-07-12 14:04:07 -0400

    added second hand

--- Assets/AnalogClockManager.cs
+++ Assets/AnalogClockManager.cs
@@ -7,21 +7,28 @@ public class AnalogClockManager : MonoBehaviour
     private Transform hourHand = null;
     [SerializeField]
     private Transform minuteHand = null;
+    [SerializeField]
+    [Tooltip("Optional Second Hand Transform")]
+    private Transform secondHand = null;
 
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float refreshRate = 0f;
     private float elapsedTime = 0f;
-    private float refreshRate = 1f;
 
     const float HOURS_ON_CLOCK = 12f;
     const float MINUTES_ON_CLOCK = 60f;
     const float SECONDS_ON_CLOCK = 60f;
+    const float MILLISECONDS_ON_CLOCK = 1000f;
     const float HOUR_DEGREES = 360f / HOURS_ON_CLOCK;
     const float MINUTE_DEGREES = 360f / MINUTES_ON_CLOCK;
+    const float SECOND_DEGREES = 360f / SECONDS_ON_CLOCK;
 
     private void Update()
     {
         elapsedTime += Time.deltaTime;
 
-        // Refresh once every second
+        // Refresh once after each refreshRate interval
         if (elapsedTime > refreshRate)
         {
             elapsedTime = 0f;
@@ -36,9 +43,14 @@ public class AnalogClockManager : MonoBehaviour
         int nHour = dt.Hour;
         int nMin = dt.Minute;
         int nSec = dt.Second;
+        int nMilSec = dt.Millisecond;
 
         SetHandRotation(hourHand, GetHourInDegrees(nHour, nMin));
         SetHandRotation(minuteHand, GetMinuteInDegrees(nMin, nSec));
+        if (secondHand != null)
+        {
+            SetHandRotation(secondHand, GetSecondInDegrees(nSec, nMilSec));
+        }
     }
 
     private void SetHandRotation(Transform hand, float degrees)
@@ -56,5 +68,10 @@ public class AnalogClockManager : MonoBehaviour
     {
         float fMinute = minute + (seconds / SECONDS_ON_CLOCK);
         return fMinute * MINUTE_DEGREES;
+    }
+    private float GetSecondInDegrees(int second, int milliseconds)
+    {
+        float fSecond = second + (milliseconds / MILLISECONDS_ON_CLOCK);
+        return fSecond * MINUTE_DEGREES;
     }
 }
